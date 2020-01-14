18ef62c6418e864f1fcd463cf3021e06057fc30f
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2020-01-14 19:30:36 +0100

    changes scaffolding

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,16 @@
 module.exports = eleventy => {
-  eleventy.addPassthroughCopy('.nojekyll')
-  eleventy.addPassthroughCopy('./style.css')
-  eleventy.addPassthroughCopy('./img')
+  eleventy.addLayoutAlias('page', 'page.njk')
+  eleventy.addLayoutAlias('article', 'article.njk')
+
+  eleventy.addPassthroughCopy('src/data')
 
   return {
-    passthroughFileCopy: true
+    passthroughFileCopy: true,
+    dir: {
+      input: 'src',
+      output: 'web',
+      includes: '_includes',
+      layouts: '_layouts'
+    }
   }
-
 }
--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 ivoputzer.blog
 ===
+My blog featuring Agile, Tech, Project Ideas, Reads, Events and everything else development-related.
