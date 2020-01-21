c232e77a1a690fe4b5b6ad64d0e40e7c79d46cbc
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2020-01-21 03:17:00 +0100

    milestone

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,42 @@
+const { URL } = require('url')
+
 const CleanCSS = require('clean-css')
-const ReadingTime = require('eleventy-plugin-reading-time');
+const readingTime = require('eleventy-plugin-reading-time');
+const moment = require('moment')
 
 module.exports = eleventy => {
+  eleventy.addPlugin(readingTime)
+
   eleventy.addLayoutAlias('page', 'page.njk')
   eleventy.addLayoutAlias('article', 'article.njk')
 
-  eleventy.addPlugin(ReadingTime)
   eleventy.addPassthroughCopy({ '_static/': '/' })
-  eleventy.addFilter('mincss', code => new CleanCSS({}).minify(code).styles)
-  eleventy.addShortcode('excerpt', excerpt)
+
+
+  eleventy.addNunjucksShortcode('excerpt', (data) => {
+    const [excerpt, _] = data.split('<hr>')
+    return excerpt
+  })
+
+  eleventy.addNunjucksShortcode('body', (data) => {
+    const [_, body] = data.split('<hr>')
+    return body
+  })
+
+  eleventy.addFilter('date', (date, format) => {
+    return moment(date).format(format)
+  })
+
+  eleventy.addNunjucksFilter('mincss', (code) => new CleanCSS({}).minify(code).styles)
+  eleventy.addNunjucksFilter('absoluteUrl', (url, base) => {
+    try {
+      const absolute = new URL(url, base)
+      return absolute.toString()
+    } catch (e) {
+      console.log('Trying to convert %o to be an absolute url with base %o and failed, returning: %o (invalid url)', url, base, url)
+      return url
+    }
+  })
 
   return {
     passthroughFileCopy: true,
@@ -19,14 +47,11 @@ module.exports = eleventy => {
   }
 }
 
-function excerpt ({ templateContent }) {
+function excerptFrom ({ templateContent }) {
   if (!templateContent) {
     console.warn('Failed to extract excerpt: Document has no property `templateContent`.');
     return
   }
 
-  return templateContent.substring(
-    templateContent.indexOf('</h1>') + 5,
-    templateContent.indexOf('<hr>')
-  ).trim()
+  return templateContent.substring(0, templateContent.indexOf('<hr>'))
 }
--- page/library.md
+++
@@ -1,9 +0,0 @@
----
-template: page
----
-
-# Library.
-
-{% for book in books %}
-1.  {{ book.title }}: {{ book.subtitle }} - {% for author in book.author %} {{ author }}{{ ',' if not loop.last }} {% endfor %}
-{% endfor %}
